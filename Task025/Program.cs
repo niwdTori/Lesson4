d8c1dadbc0bd096796845b6d6ccf279af5b71751
Tori <mes.viktoria@mail.ru>
2022-09-02 22:08:18 +0500

    Возведение числа А в степень В

---
+++ Task025/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+int sum = 1;
+
+for(int i = 1; i <=b; i++)
+{
+    sum = sum * a;
+}
+Console.WriteLine(sum);
